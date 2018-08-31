93ede5674086718154832da22a767d2779c684cd
jerrita <782981223@qq.com>
2018-08-31 12:49:40 +0800

    第一版

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "欢迎使用termux一键配置脚本！"
+echo -e "版本：v1.0    作者：紫泉梦月\n"
+echo -e "正在进行安装...请稍后...\n正在更换软件源..."
+sed -ie '2cdeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main' /data/data/com.termux/files/usr/etc/apt/sources.list
+echo -e "更换成功！更新中..."
+apt update
+echo "安装必备组件中..."
+apt install -y git wget curl zsh python vim
+echo "安装完成！准备美化脚本中..."
+echo "提示：下列操作国内可能较慢，可准备梯子以进行加速，准备好请输入任意字符..."
+read tmp
+cd ~
+git clone http://github.com/jerrita/termux
+echo "正在进行vim配置&美化..."
+cp ~/termux/src/vimrc ~/.vimrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+echo "正在执行最后操作，美化桌面中（若需要输入，回车就行）..."
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
